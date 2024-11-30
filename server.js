f7859a6df5ec755612600a454154fb809d8daa6d
Tajul Islam Sumon <38192828+Tajul-Islam-Sumon@users.noreply.github.com>
2024-11-30 19:00:11 +0600

    server.js

--- server.js
+++ server.js
@@ -8,36 +8,42 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-//confugure dotenv
+// Configure dotenv
 dotenv.config();
 
-//database config
+// Database config
 connectDB();
 
-//rest object
+// Get the current directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Rest object
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-//routes
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
+// Rest API
 app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//port
+// Port
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// Run listen
 app.listen(PORT, () => {
   console.log(
     `Server running on ${process.env.DEV_MODE} mode on port ${PORT} successfully.`
